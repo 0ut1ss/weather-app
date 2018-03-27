95339de82757b00ad3b01052fa742f115350d403
0ut1ss <dngroumpos@hotmail.com>
2018-03-27 08:59:39 +0300

    Initial commit

---
+++ src/components/WeatherApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Loader from './Loader';
+
+export default class WeatherApp extends React.Component {
+
+
+    render () {
+        return (
+            <div>
+                <Loader />
+            </div>
+        );
+    }
+}
